ecc2483b140f87660a0453bcaa493590a3476d4a
Potter <Robin.Potter@experian.com>
2019-11-07 14:11:17 +0000

    moved of mini canvases to one gib one, if works..

--- tools/lib/functions.js
+++ tools/lib/functions.js
@@ -15,9 +15,10 @@ var colour_list_canvas
 var sprite_offset_x 
 var sprite_offset_y   
 
-var cs, ss
+var current_sprite_canvas
+var current_map_canvas
 
-var tile_index=0
+var current_tile_index=0
 var max_tiles = 5
 var colour_index=0
 var max_colours = 4
@@ -49,16 +50,16 @@ function add_new_sprite() {
     
 }
 
-function update_map(element, object) {
-    var ctx = element.node().getContext("2d")
-    ctx.drawImage(sprite_list_canvas, tile_index*8,0,8,8,0, 0,8,8);
-}
+//function update_map(element, object) {
+//    var ctx = element.node().getContext("2d")
+//    ctx.drawImage(sprite_list_canvas, current_tile_index*8,0,8,8,0, 0,8,8);/
+//}
 
-function update_sprite(element, object) {
-    console.log(element.node(), object)
-    var ctx = element.node().getContext("2d")
-    ctx.drawImage(colour_list_canvas, colour_index*8,0,8,8,0, 0,8,8);
-}
+//function update_sprite(element, object) {
+// /   console.log(element.node(), object)
+//    var ctx = element.node().getContext("2d")
+//    ctx.drawImage(colour_list_canvas, colour_index*8,0,8,8,0, 0,8,8);
+//}
 
 function update_sprite_canvases() {
     ss.each(function(d,i) {
@@ -99,6 +100,12 @@ function setup_sprite_ui() {
         .attr('type','button')
         .on('click',add_new_sprite)
 
+    current_sprite_canvas = d3.select('body').append('canvas')
+        .attr('id','current_sprite_canvas')
+        .style('width',`${SW * SCHO}px`)
+        .style('height',`${SH * SCHO}px`)
+        .style('position','absolute')
+
 }
 
 function setup_map_ui() {
@@ -127,17 +134,47 @@ function setup_map_ui() {
         //.style('top',`${offset_y-2}px`)
         //.style('posleftition',`${offset_x-2}`)
 
-    }
+    current_map_canvas = d3.select('body').append('canvas')
+        .attr('id','current_map_canvas')
+        .style('width',`${W * CHO}px`)
+        .style('height',`${H * CHO}px`)
+        .style('position','absolute')
+        .on('mousedown', function() {
+            console.log('mouse down',d3.event,d3.event.offsetX,d3.event.offsetY)
+            mx=d3.event.offsetX
+            my=d3.event.offsetY
+            cx=Math.abs(Math.floor(mx/CHO))
+            cy=Math.abs(Math.floor(my/CHO))
+            cell = current_map.filter(function(m,i) { if (m.x==cx & m.y==cy) return m })[0]          
+            console.log(cx,cy,cell)
+            cell.value=current_tile_index
+            update_map_canvas()            
+        })
+
+}
+
+function update_map_canvas() {
+    current_map.forEach(function(cell) {
+        console.log(cell)
+        //    var ctx = element.node().getContext("2d")
+        //    ctx.drawImage(colour_list_canvas, colour_index*8,0,8,8,0, 0,8,8);
+        var ctx = current_map_canvas.node().getContext("2d")
+        ctx.drawImage(sprite_list_canvas, cell.value * 8, 0, 8, 8, cell.x*CHO, cell.y*CHO,8,8);
+
+    })
+
+
+}
 
 function setup_map_ui_action() {
 
     //set up ui actions
     d3.select('#tiles').on('mouseup', function() {
-        //console.log(tile_index)
-        tile_index++
-        if (tile_index==max_tiles) tile_index=0
+        //console.log(current_tile_index)
+        current_tile_index++
+        if (current_tile_index==max_tiles) current_tile_index=0
         var ctx = document.getElementById('current_tile').getContext('2d')
-        ctx.drawImage(tiles, tile_index*8, 0, 8, 8,
+        ctx.drawImage(tiles, current_tile_index*8, 0, 8, 8,
             0, 0, 32, 32)
     })
 }
@@ -195,58 +232,15 @@ function gogogo() {
 
     initialize_map()
 
-    //create map cells
+    current_sprite_canvas.style('top',`${sprite_offset_y}px`)
+    current_sprite_canvas.style('left',`${sprite_offset_x}px`)
+    current_map_canvas.style('top',`${map_offset_y}px`)
+    current_map_canvas.style('left',`${map_offset_x}px`)
 
-    cs = d3.select('body').selectAll('.cell').data(current_map).enter()
-
-    cs
-        .append('canvas')
-        .attr('class','cell')
-        .attr('width','8px')
-        .attr('height','8px')
-        .style('position','absolute')
-        .style('top',function(d) { return `${ map_offset_y + CHO*d.y}px`})
-        .style('left',function(d) { return `${ map_offset_x + CHO*d.x}px`})
-        //.style('border','solid')
-        //.style('border-width','1px')
-        .on('mousedown', function(d,i) {
-            //disbale dragg
-            d3.event.preventDefault ? d3.event.preventDefault() : d3.event.returnValue = false
-            d.value=tile_index
-            if (d3.event.buttons==1) update_map(d3.select(this), d)
-        })
-        .on('mouseenter', function(d,i) {
-            //console.log("mounse enter", d3.event)
-            if (d3.event.buttons==1) {
-                d.value=tile_index
-                update_map(d3.select(this), d)
-            }
-        })
+    //create map cells  
 
 
     //create sprite colour cells
     sprite = current_sprites[current_sprite_index]
 
-    ss = d3.select('body').selectAll('.spr_cell').data(sprite).enter()
-        .append('canvas')
-        .attr('class','spr_cell')
-        .attr('width','8px')
-        .attr('height','8px')
-        .style('position','absolute')
-        .style('top',function(d) { return `${ sprite_offset_y + SCHO*d.y}px`})
-        .style('left',function(d) { return `${ sprite_offset_x + SCHO*d.x}px`})
-        .on('mousedown', function(d,i) {
-            //disbale dragg
-            console.log('sprite mouse down')
-            d3.event.preventDefault ? d3.event.preventDefault() : d3.event.returnValue = false
-            d.value=colour_index
-            if (d3.event.buttons==1) update_sprite(d3.select(this), d)
-        })
-        .on('mouseenter', function(d,i) {
-            //console.log("mounse enter", d3.event)
-            if (d3.event.buttons==1) {
-                d.value=colour_index
-                update_sprite(d3.select(this), d)
-            }
-        })
 }
