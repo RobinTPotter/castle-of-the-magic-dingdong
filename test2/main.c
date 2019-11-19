5f30cdee10352b1a3a5afb2094f6e46a92f22e79
rtp <>
2019-11-19 15:07:59 +0000

    test2

--- test2/main.c
+++ test2/main.c
@@ -1,15 +1,25 @@
 #include <gb/gb.h>
 #include "res.c"
+#include <stdio.h>
+
+    UINT8 tick = 0;
 
 void main(){
-    set_bkg_data(0, 4, spr);
-    set_bkg_tiles(0, 0, 40, 40, map);
+    set_bkg_data(0, 21, spr);
+    set_bkg_tiles(0, 0, 32, 32, map);
 
     SHOW_BKG;
     DISPLAY_ON;
 
+
     while(1){
-        scroll_bkg(1,1);
+
+        scroll_bkg(1,0);
+
+        if (tick % 2==0) scroll_bkg(0,1);
+
         delay(100);
+        tick++;
+        if (tick>200) tick=0;
     }
 }
